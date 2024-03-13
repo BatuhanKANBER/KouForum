f8d644f86fdc2c42966ce4041cd5c70c41ea7663
Batuhan KANBER <batuhankanber28@gmail.com>
2024-03-13 15:47:12 +0300

    feat : apiProgress and successMessage added.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { SignUp } from './pages/SignUp/signup.jsx'
-import { Login } from './pages/Login/login.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SignUp />
--- frontend/src/pages/SignUp/signup.jsx
+++ frontend/src/pages/SignUp/signup.jsx
@@ -1,36 +1,50 @@
 import { useState } from 'react'
-import axios from 'axios'
 import './signup.css'
+import { signUp } from './api'
 export function SignUp() {
     const [username, setUsername] = useState()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [passwordConfirm, setPasswordConfirm] = useState()
+    const [apiProgress, setApiProgress] = useState(false)
+    const [successMessage, setSuccessMessage] = useState()
 
-    console.log(username)
-    console.log(email)
-    console.log(password)
-    console.log(passwordConfirm)
-
-    const onSubmit = (event) =>{
+    const onSubmit = async (event) => {
         event.preventDefault();
-       axios.post("/api/users/create", {
-        username,
-        email,
-        password
-       })
+        setSuccessMessage();
+        setApiProgress(true);
+
+        try {
+            const response = await signUp({
+                username,
+                email,
+                password
+            })
+            setSuccessMessage(response.data.message)
+        } catch {
+
+        } finally {
+            setApiProgress(false)
+        }
     }
 
 
     return <>
         <div className="container" id="container">
+
             <div className="form-container sign-up-container">
+
                 <form onSubmit={onSubmit}>
                     <input id="username" onChange={(event) => setUsername(event.target.value)} type="text" placeholder="Kullanıcı Adı" />
                     <input id="email" onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" />
                     <input id="password" onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Parola" />
                     <input id="passwordConfirm" onChange={(event) => setPasswordConfirm(event.target.value)} type="password" placeholder="Parolayı Onayla" />
-                    <button disabled={password!=passwordConfirm && password!=null} type='submit'>Kayıt Ol</button>
+                    <button disabled={apiProgress || (password != passwordConfirm || !password)} type='submit'>
+                        {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+                        Kayıt Ol
+                    </button>
+                    <br></br>
+                    {successMessage && <div className="alert alert-success" role="alert">{successMessage}</div>}
                 </form>
             </div>
             <div className="overlay-container">
