b24ab57334115a7a5332838ad3056418439502c4
Batuhan KANBER <batuhankanber28@gmail.com>
2024-03-15 16:59:20 +0300

    code refactoring

--- backend/src/main/java/com/kouforum/backend/controllers/UserController.java
+++ backend/src/main/java/com/kouforum/backend/controllers/UserController.java
@@ -2,9 +2,14 @@ package com.kouforum.backend.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +20,7 @@ import com.kouforum.backend.models.User;
 import com.kouforum.backend.services.UserService;
 import com.kouforum.backend.shared.GenericMessage;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,27 +29,23 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    //USER CREATE
     @PostMapping("/create")
-    ResponseEntity<?> createUser(@RequestBody User user) {
+    GenericMessage createUser(@Valid @RequestBody User user) {
+        userService.save(user);
+        return new GenericMessage("User is created.");
+    }
 
+    //VALID OPERATIONS
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception) {
         ApiError apiError = new ApiError();
         apiError.setPath("/api/users/create");
         apiError.setMessage("Validation error");
         apiError.setStatus(400);
-        Map<String, String> validationErrors = new HashMap<>();
-        if (user.getUsername() == null || user.getUsername().isEmpty()) {
-            validationErrors.put("username", "Username cannot be null");
-        }
-
-        if (user.getEmail() == null || user.getEmail().isEmpty()) {
-            validationErrors.put("email", "Email cannot be null");
-        }
-        if (validationErrors.size() > 0) {
-            apiError.setValidationErrors(validationErrors);
-            return ResponseEntity.badRequest().body(apiError);
-        }
-
-        userService.save(user);
-        return ResponseEntity.ok(new GenericMessage("User is created."));
+        var validationErrors = exception.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        apiError.setValidationErrors(validationErrors);
+        return ResponseEntity.badRequest().body(apiError);
     }
 }
