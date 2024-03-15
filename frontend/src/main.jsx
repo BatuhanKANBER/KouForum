1e9f112b526b32036b1e92f3adda3f39a847a14a
Batuhan KANBER <batuhankanber28@gmail.com>
2024-03-15 04:27:31 +0300

    code refactoring for signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { SignUp } from './pages/SignUp/signup.jsx'
+import "./styles.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- frontend/src/pages/SignUp/signup.jsx
+++ frontend/src/pages/SignUp/signup.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './signup.css'
 import { signUp } from './api'
 export function SignUp() {
     const [username, setUsername] = useState()
@@ -38,33 +37,72 @@ export function SignUp() {
 
 
     return <>
-        <div className="container" id="container">
+        <div className="container pt-5">
+            <div className="card">
+                <div className="card-body">
+                    <form onSubmit={onSubmit}>
+                        <div className="mb-3 d-flex justify-content-center">
+                            <h3>KAYIT OL</h3>
+                        </div>
+                        <div className="mb-3">
+                            <input id="username" className={errors.username ? "form-control is-invalid" : "form-control is-valid"} onChange={(event) => setUsername(event.target.value)} type="text" placeholder="Kullanıcı Adı" />
+                            <div className="invalid-feedback">{errors.username}</div>
+                        </div>
+                        <div className="mb-3">
 
-            <div className="form-container sign-up-container">
+                            <input id="email" className="form-control" onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" />
+                        </div>  <div className="mb-3">
 
-                <form onSubmit={onSubmit}>
-                    <input id="username" className={errors.username ? "form-control is-invalid" : "form-control is-valid"} onChange={(event) => setUsername(event.target.value)} type="text" placeholder="Kullanıcı Adı" />
-                    <div className="invalid-feedback">{errors.username}</div>
-                    <input id="email" onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" />
-                    <input id="password" onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Parola" />
-                    <input id="passwordConfirm" onChange={(event) => setPasswordConfirm(event.target.value)} type="password" placeholder="Parolayı Onayla" />
-                    <button disabled={apiProgress || (password != passwordConfirm || !password)} type='submit'>
-                        {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
-                        Kayıt Ol
-                    </button>
+                            <input id="password" className="form-control" onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Parola" />
+                        </div>  <div className="mb-3">
+
+                            <input id="passwordConfirm" className="form-control" onChange={(event) => setPasswordConfirm(event.target.value)} type="password" placeholder="Parolayı Onayla" />
+                        </div>  <div className="mb-3">
+
+                            <button className="btn btn-primary text-white" disabled={apiProgress || (password != passwordConfirm || !password)} type='submit'>
+                                {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+                                Kayıt Ol
+                            </button>
+                        </div>
+                        <br></br>
+                        {successMessage && <div className="alert alert-success" role="alert">{successMessage}</div>}
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        {/* <div className="card">
+            <div className="card-body">
+                <div className="card-header d-flex justify-content-center">
+                    KAYIT OL
+                </div>
+                <form onSubmit={onSubmit} className=''>
+                    <div className="mb-3">
+
+                        <input id="username" className={errors.username ? "form-control is-invalid" : "form-control is-valid"} onChange={(event) => setUsername(event.target.value)} type="text" placeholder="Kullanıcı Adı" />
+                        <div className="invalid-feedback">{errors.username}</div>
+                    </div>
+                    <div className="mb-3">
+
+                        <input id="email" onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" />
+                    </div>  <div className="mb-3">
+
+                        <input id="password" onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Parola" />
+                    </div>  <div className="mb-3">
+
+                        <input id="passwordConfirm" onChange={(event) => setPasswordConfirm(event.target.value)} type="password" placeholder="Parolayı Onayla" />
+                    </div>  <div className="mb-3">
+
+                        <button disabled={apiProgress || (password != passwordConfirm || !password)} type='submit'>
+                            {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+                            Kayıt Ol
+                        </button>
+                    </div>
                     <br></br>
                     {successMessage && <div className="alert alert-success" role="alert">{successMessage}</div>}
                 </form>
             </div>
-            <div className="overlay-container">
-                <div className="overlay">
-                    <div className="overlay-panel overlay-right">
-                        <h1>Hoşgeldiniz!</h1>
-                        <p>Aramıza katılabilmek için lütfen hesap oluşturunuz.</p>
-                        <button className="ghost" id="signIn">Giriş Yap</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> */}
+
     </>
 }
