965a8c893acaa0f1c8254b1badee8c851465ce7f
Batuhan KANBER <batuhankanber28@gmail.com>
2024-04-08 02:52:51 +0300

    feat : fake users function is created.

--- backend/src/main/java/com/kouforum/backend/BackendApplication.java
+++ backend/src/main/java/com/kouforum/backend/BackendApplication.java
@@ -1,8 +1,15 @@
 package com.kouforum.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.kouforum.backend.models.User;
+import com.kouforum.backend.repositories.UserRepository;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
@@ -12,4 +19,19 @@ public class BackendApplication {
 		System.out.println("Hello World!");
 	}
 
+	@Bean
+	CommandLineRunner fakeUsers(UserRepository userRepository) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return (args) -> {
+			for (var i = 1; i <= 28; i++) {
+				User user = new User();
+				user.setUsername("fake_user" + i);
+				user.setEmail("user" + i + "@gmail.com");
+				user.setPassword(passwordEncoder.encode("Password123"));
+				user.setIs_active(true);
+				userRepository.save(user);
+				System.out.println(user.getUsername() + " is created.");
+			}
+		};
+	}
 }
